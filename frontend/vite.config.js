98e7024538cd5d236de3a9ae55b21c26ab0b7b84
Newweehub <nutnichacha67@gmail.com>
2026-05-06 19:05:23 +0700

    config deployed backend to frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://serenity-backend-c4cxeedyfahpfpac.southeastasia-01.azurewebsites.net', //'http://localhost:3001'
         changeOrigin: true,
       },
     },
